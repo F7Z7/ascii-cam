4a64fcff101d8c5dc0eff5bc15e318fcc3ead762
farza <farzanrichu610@gmail.com>
2025-06-13 01:00:11 +0530

    set up canvas

--- scripts/ascii.js
+++ scripts/ascii.js
@@ -1,4 +1,16 @@
-let start=document.getElementById("startButton")
-start.addEventListener("click",()=>{
-    console.log("start");
-})
+const video=document.getElementById('video');
+const canvas=document.getElementById('canvas');
+const ctx=canvas.getContext('2d'); //for images
+const asciiOutput=document.getElementById('asciiOutput');
+
+const asciiChars = "@#W$9876543210?!abc;:+=-,._ ";
+
+let asciiInterval
+
+function convertToAscii(){
+    asciiOutput.style.display="block";
+    video.style.visibility="hidden";
+    canvas.width=100
+    canvas.height=100
+
+}
