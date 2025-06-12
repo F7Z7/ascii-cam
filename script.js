76b9bcd1e648da877b579003e22c8419ecf962b7
farza <farzanrichu610@gmail.com>
2025-06-12 21:11:07 +0530

    basic camera on and off done

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function stopVideo(){
     const video=document.getElementById('video');
     const stream=video.srcObject;
+    if(!stream) return;
+
     const tracks=stream.getTracks();
     let track
-    for (track in tracks){
-        track.stop()
-    }
+    tracks.forEach(track => track.stop());
     video.srcObject = null;
+}
+function startVideo(){
+    const video=document.getElementById('video');
+    if(navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({video:true}) //user allows
+        .then(stream=>{
+            video.srcObject = stream;
+        })
+            .catch(error=>{
+                console.error(error);
+            })
+    }
+    else{
+        console.log("getUserMedia not supported on your browser!");
+    }
 }
